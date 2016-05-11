ec9c8334da444ed09a15918a8ae710adfb1d589e
manugupta9228 <manu.gupta@oodlestechnologies.com>
2016-05-11 16:25:53 +0530

    added unwind segue project

---
+++ unwindSegue/unwindSegue/ScreenOneViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ScreenOneViewController.swift
+//  unwindSegue
+//
+//  Created by manu.oodles on 5/11/16.
+//  Copyright © 2016 manu.oodles. All rights reserved.
+//
+
+import UIKit
+
+class ScreenOneViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func unwindToScreenOne(segue: UIStoryboardSegue) {
+        
+    }
+
+}
---
+++ unwindSegue/unwindSegue/ScreenTwoViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ScreenTwoViewController.swift
+//  unwindSegue
+//
+//  Created by manu.oodles on 5/11/16.
+//  Copyright © 2016 manu.oodles. All rights reserved.
+//
+
+import UIKit
+
+class ScreenTwoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func unwindToScreenTwo(segue: UIStoryboardSegue) {
+        
+    }
+
+}
